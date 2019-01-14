e3c97b3173217f2002eba155383c5a9e5867335d
mepyyeti <rcabej@gmail.com>
2019-01-13 20:49:46 -0500

    introduce search function

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # password_genie -- 
-a tool that 1. generates a custom password based on a user's specifications and 2. store password and corresponding usernames and site names
+a tool that 1. generates a custom password based on a user's specifications 2. store password and corresponding usernames and site names and 3. introduces a search function to retrieve user info
--- lib/password_genie.rb
+++ lib/password_genie.rb
@@ -48,16 +48,14 @@ class WordBank
 	def create
 		i = number
 		while i != 0
-			if i == 2 && (@password_ary.include?([@special_chars]) == false && word_bank.include?(@special_chars))
-					@password_ary << @special_chars[rand(1..@special_chars.size)]
-			elsif i == 1 && (@password_ary.include?(@numbers) == false && word_bank.include?(@numbers))
+			if i == 2 && word_bank.include?(@special_chars) && @password_ary.include?(@special_chars) == false
+					@password_ary << @special_chars[rand(0..@special_chars.size)]
+					puts "1 == 2"
+			elsif i == 1 && word_bank.include?(@numbers) && @password_ary.include?(@numbers) == false
 					@password_ary << @numbers[rand(1..@numbers.size)]
+					puts "i == 1"
 			end
-			letter = rand(1..word_bank.size)
-			if letter == nil
-				next
-			end
-			puts "letter: #{word_bank[letter]} and num: #{letter}"
+			letter = rand(0..word_bank.size)
 			@password_ary << word_bank[letter]
 			word_bank.delete_at(letter)
 			i -= 1
@@ -73,50 +71,73 @@ class WordBank
 			f.puts("[#{site}] username: #{username} | password: #{@password}")
 		end
 	end
+
+	def find_info(info)
+		return "please create a password directory first" unless File.exist?('site_info.txt')
+			IO.foreach('site_info.txt') do |line|
+				return line if line.include?(info)
+				end
+			return "no match"
+	end
 end
 
 go = true
 
 while go
-	puts "HELLO. You will now design a custom password!"
-	puts "How long do you want your password to be?"
+	puts "enter [1] to CREATE a new password\nenter [2] to LOOKUP a password."
 	
-	number = gets.chomp.to_i
-	if number == 1
-		word_number = "character"
+	choice = gets.chomp.to_i
+	
+	if choice == 1
+		puts "ou will now design a custom password!"
+		puts "How long do you want your password to be?"
+		
+		number = gets.chomp.to_i
+		if number == 1
+			word_number = "character"
+		else
+			word_number = "characters"
+		end
+		puts "your word bank will contain #{number} #{word_number}."
+		
+		puts "please compile your word bank:"
+		words = gets.chomp
+		
+		x = WordBank.new(words, number)
+		
+		puts 'do you wish to include special characters such as "." "," and "$"? [y/n]'
+		special_char = gets.chomp
+		x.add_special_chars unless special_char == "n" 
+		puts "do you wish to include numbers? [y/n]"
+		num_chc = gets.chomp
+		x.add_numbers unless num_chc == 'n' 
+		x.create
+		
+		puts "done.\n\n"
+		puts "\tto save, please enter 'save'"
+		choice = gets.chomp.downcase
+		if choice == 'save' || choice == 's' || choice == 'yes' || choice == 'y'
+			puts "enter site"
+			site = gets.chomp.downcase
+			puts "enter username"
+			username = gets.chomp.to_s
+			x.save_info(site,username)
+		end
+		
+		puts "go again? [y/n]"
+		choice = gets.chomp
+		unless choice == 'y'
+			go = false
+		end
+		next
+	elsif choice == 2
+		print "enter the word you want to search for (usually site or username):  "
+		word = gets.chomp.to_s
+		x = WordBank.new(word)
+		puts x.find_info(word)
+		next
 	else
-		word_number = "characters"
-	end
-	puts "your word bank will contain #{number} #{word_number}."
-	
-	puts "please compile your word bank:"
-	words = gets.chomp
-	
-	x = WordBank.new(words, number)
-	
-	puts 'do you wish to include special characters such as "." "," and "$"? [y/n]'
-	special_char = gets.chomp
-	x.add_special_chars unless special_char == "n" 
-	puts "do you wish to include numbers? [y/n]"
-	num_chc = gets.chomp
-	x.add_numbers unless num_chc == 'n' 
-	x.create
-	
-	puts "done.\n\n"
-	puts "\tto save, please enter 'save'"
-	choice = gets.chomp.downcase
-	if choice == 'save' || choice == 's' || choice == 'yes' || choice == 'y'
-		puts "enter site"
-		site = gets.chomp.downcase
-		puts "enter username"
-		username = gets.chomp.to_s
-		x.save_info(site,username)
+		next
 	end
 	
-	puts "go again? [y/n]"
-	choice = gets.chomp
-	unless choice == 'y'
-		go = false
-	end
-	next
 end
