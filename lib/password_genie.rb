a1e861283880a486155758a35510b91d5e3ca4c0
mepyyeti <rcabej@gmail.com>
2019-01-14 23:02:14 -0500

    added puts function for existing password, site, username combos

--- lib/password_genie.rb
+++ lib/password_genie.rb
@@ -5,7 +5,7 @@ class WordBank
 	attr_reader :word_bank, :number, :special_chars
 	
 	def word_bank=(words)
-		word_bank=words.split("").to_a
+		word_bank= words.split("").to_a
 		word_bank.delete(" ")
 		unless word_bank.is_a?(Array)
 			raise "word bank must contain letters"
@@ -79,17 +79,24 @@ class WordBank
 				end
 			return "no match"
 	end
+	
+	def push_info(site,username,password)
+		my_file = File.open('site_info.txt', 'a+') do |f|
+			f.puts("[#{site}] username: #{username} | password: #{password}")
+			end
+	end
 end
 
 go = true
 
 while go
-	puts "enter [1] to CREATE a new password\nenter [2] to LOOKUP a password."
+	puts "enter [1] to CREATE a new password\nenter [2] to LOOKUP a password"
+	puts "enter [3] to ADD an already existing password\nenter [4] to EXIT"
 	
 	choice = gets.chomp.to_i
 	
 	if choice == 1
-		puts "ou will now design a custom password!"
+		puts "you will now design a custom password!"
 		puts "How long do you want your password to be?"
 		
 		number = gets.chomp.to_i
@@ -136,6 +143,18 @@ while go
 		x = WordBank.new(word)
 		puts x.find_info(word)
 		next
+	elsif choice == 3
+		print "enter site: "
+		site = gets.chomp
+		print "enter username: "
+		username = gets.chomp
+		print "enter password: "
+		password = gets.chomp
+		x = WordBank.new(site)
+		x.push_info(site,username,password)
+		next
+	elsif choice == 4
+		go = false
 	else
 		next
 	end
